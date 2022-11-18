50f9d540455bb375994e0a6bf1eadbf0cfb57623
olserra <olserra@gmail.com>
2022-11-18 12:17:55 +0000

    adding analytics and removing session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ export default function App({
     <ThemeProvider attribute="class">
       <main className={interVariable.className}>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-E5NV41KVYK"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z3HJ9S40WJ"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -24,7 +24,7 @@ export default function App({
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-E5NV41KVYK');
+          gtag('config', 'G-Z3HJ9S40WJ');
         `}
         </Script>
         <Component {...pageProps} />
